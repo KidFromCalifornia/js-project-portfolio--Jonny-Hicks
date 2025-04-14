cff8fdb83f401f53bf62ecd6a1e9bf0edbca855e
kidfromcalifornia <hello.jonnyhicks@gmail.com>
2025-04-14 15:08:00 +0200

    CSS stying started  and link fonts and icons

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import { App } from './App.jsx'
+import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 import './index.css'
 

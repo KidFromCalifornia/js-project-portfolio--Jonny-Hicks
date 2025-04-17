53ae1c15b9ec5ccc2f2dcdc4aafbda5091c167ef
kidfromcalifornia <hello.jonnyhicks@gmail.com>
2025-04-17 11:14:07 +0200

    reanmed folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./index.css"
 import "font-awesome/css/font-awesome.min.css"
-import AboutMe from "./Sections/Main/About/AboutMe.jsx"
+import {AboutMe} from "./Sections/Main/About/AboutMe.jsx"
 
 
 export const App = () => {
